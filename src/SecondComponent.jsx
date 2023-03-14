72559b4c2e89cb9213d480168429eb2194185239
ismytv <ismytv@gmail.com>
2023-03-14 16:40:20 -0500

    create second component

---
+++ src/SecondComponent.jsx
@@ -0,0 +1,6 @@
+export const SecondComponent = () => {
+  return (
+    <h1>My seconf component!!!</h1>
+  )
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { SecondComponent } from "./SecondComponent";
 
 
 ReactDOM.createRoot( document.getElementById('root' ) ).render(
     <React.StrictMode>
         <App/>
+        <SecondComponent/>
     </React.StrictMode>
 )
