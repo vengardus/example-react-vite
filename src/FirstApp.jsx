6489300d1f70e80df9975a0bbd615f985fc1bbbf
ismytv <ismytv@gmail.com>
2023-03-14 17:36:01 -0500

    printing variables in components

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 
+
+const myVar = "mi variable"
+const myObject = {
+    name: 'Vengardus',
+    country: 'Peru'
+}
+
+const myFunc = (name) => {
+    return `Hola ${name}`
+}
+
 export default function FirstApp() {
   return (
     <>
-        <h1>Titulo</h1>
-        <div>Ejemplo de fragment</div>
-        <div>Solo debe existir un nodtilizapadre</div>
-        <h2>Para ello se utiliza una etiqueta vacia al inicio y final del grupo de nodos </h2>
+        <h1>Fragment</h1>
+        <div>Solo debe existir un nodo o elemento padre</div>
+        <div>Si hay varios nodos a un mismo nivel, deben ir dentro de un nodo "fragment": {'<> </>'}</div>
+
+        <h1>Impresión de variables</h1>
+        <div>Las variables se imprimen dentro de llaves.</div>
+        <div>Dentro de las llaves puede ir cualquier expresion Js, excepto objetos (directamente)</div>
+        <div>Esta es una variable: { myVar }</div>
+        <div>Este es un objeto serializado: { JSON.stringify(myObject) }</div>
+        <div>Esta es una propiedad del objeto: { myObject.name }</div>
+        <div>Este es un valor retornado de una función: { myFunc('Gardus') }</div>
+
     </>
   )
 }
