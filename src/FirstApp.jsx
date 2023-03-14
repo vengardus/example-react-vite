c9c1608db01760d1da142a15bf42622b7132c4cb
ismytv <ismytv@gmail.com>
2023-03-14 16:54:53 -0500

    elemento fragment

---
+++ src/FirstApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function FirstApp() {
+  return (
+    <>
+        <h1>Titulo</h1>
+        <div>Ejemplo de fragment</div>
+        <div>Solo debe existir un nodtilizapadre</div>
+        <h2>Para ello se utiliza una etiqueta vacia al inicio y final del grupo de nodos </h2>
+    </>
+  )
+}
