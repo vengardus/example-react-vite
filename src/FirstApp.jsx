64a25f12c252da44599680e305c316a24f72d0b7
ismytv <ismytv@gmail.com>
2023-03-15 14:56:45 -0500

    Props: communication between components

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -3,28 +3,33 @@ import React from 'react'
 
 const myVar = "mi variable"
 const myObject = {
-    name: 'Vengardus',
-    country: 'Peru'
+  name: 'Vengardus',
+  country: 'Peru'
 }
 
 const myFunc = (name) => {
-    return `Hola ${name}`
+  return `Hola ${name}`
 }
 
-export default function FirstApp() {
+export default function FirstApp({ name, number }) {
   return (
     <>
-        <h1>Fragment</h1>
-        <div>Solo debe existir un nodo o elemento padre</div>
-        <div>Si hay varios nodos a un mismo nivel, deben ir dentro de un nodo "fragment": {'<> </>'}</div>
+      <h1>Props</h1>
+      <div>Hola { name }, tu número es { number }</div>
+      <div>Y tu número multiplicado por 2 es { number*2 }</div>
 
-        <h1>Impresión de variables</h1>
-        <div>Las variables se imprimen dentro de llaves.</div>
-        <div>Dentro de las llaves puede ir cualquier expresion Js, excepto objetos (directamente)</div>
-        <div>Esta es una variable: { myVar }</div>
-        <div>Este es un objeto serializado: { JSON.stringify(myObject) }</div>
-        <div>Esta es una propiedad del objeto: { myObject.name }</div>
-        <div>Este es un valor retornado de una función: { myFunc('Gardus') }</div>
+
+      <h1>Fragment</h1>
+      <div>Solo debe existir un nodo o elemento padre</div>
+      <div>Si hay varios nodos a un mismo nivel, deben ir dentro de un nodo "fragment": {'<> </>'}</div>
+
+      <h1>Impresión de variables</h1>
+      <div>Las variables se imprimen dentro de llaves.</div>
+      <div>Dentro de las llaves puede ir cualquier expresion Js, excepto objetos (directamente)</div>
+      <div>Esta es una variable: {myVar}</div>
+      <div>Este es un objeto serializado: {JSON.stringify(myObject)}</div>
+      <div>Esta es una propiedad del objeto: {myObject.name}</div>
+      <div>Este es un valor retornado de una función: {myFunc('Gardus')}</div>
 
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import './styles.css'
 
 ReactDOM.createRoot( document.getElementById('root' ) ).render(
     <React.StrictMode>
-        <FirstApp/>
+        <FirstApp name='Gardus' number={ 27 } />
     </React.StrictMode>
 )
