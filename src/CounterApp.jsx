b604187d4d266aa1dc674aaebcdb7885c0f8aa7e
ismytv <ismytv@gmail.com>
2023-03-15 15:54:57 -0500

    Click event

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,11 +1,20 @@
 import PropTypes from 'prop-types'
 
 
+const handleEvent = (event) => {
+    console.log(event)
+} 
+
 export const CounterApp = ( { value }) => {
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
+
+            <button onClick={ handleEvent }>
+                +1
+            </button>
         </>
     )
 }
