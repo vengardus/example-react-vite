4d4a37715903807c402976debe834e5c765bd2dc
ismytv <ismytv@gmail.com>
2023-03-15 16:11:01 -0500

    useState Hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,16 +1,20 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 
-const handleEvent = (event) => {
-    console.log(event)
-} 
 
 export const CounterApp = ( { value }) => {
+    const [ counter, setCounter] = useState(value)
+    
+    const handleEvent = (event) => {
+        console.log(event)
+        setCounter( counter + 1)
+    } 
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{ counter }</h2>
 
             <button onClick={ handleEvent }>
                 +1
