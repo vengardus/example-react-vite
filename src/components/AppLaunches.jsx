2d0b6f0c25cfd9420c1ca74c5eb5af37b96cf7b6
ismytv <ismytv@gmail.com>
2023-03-16 17:47:51 -0500

    add chakra UI component

--- src/components/AppLaunches.jsx
+++ src/components/AppLaunches.jsx
@@ -1,29 +1,68 @@
 import { useState, useEffect } from "react"
 import { getAllLaunches } from "../services/launches"
 import logo from "../assets/img/logo.png"
+import { 
+  Box, 
+  Image, 
+  Heading, 
+  Tag, 
+  Flex, 
+  Text, 
+  Spacer } from '@chakra-ui/react'
 
 
 export const AppLaunches = () => {
-    const [launches, setLaunches] = useState([])
+  const [launches, setLaunches] = useState([])
 
-    useEffect(() => {
-        getAllLaunches().then(data => setLaunches(data))
-    }, [])
+  useEffect(() => {
+    getAllLaunches().then(data => setLaunches(data))
+  }, [])
 
-    return (
-        <>
-            <img src={logo} width={200}></img>
-            <h1>All Launches</h1>
-            <ul>
-                {
-                    // flight_number is not unique
-                    launches.map(launche => (
-                        <li key={ launche.flight_number+launche.mission_name }>
-                            { launche.mission_name }  ( { launche.launch_year } )
-                        </li>
-                    ))
-                }
-            </ul>
-        </>
-    )
+  return (
+    <>
+      <Box mx="4rem">
+        <Image
+          src={logo}
+          boxSize="150px"
+        />
+
+        <Heading
+          size="lg"
+          mb="1rem"
+        >
+          All Launches
+        </Heading>
+
+        <section pt="1rem">
+          {
+            // flight_number is not unique
+            launches.map(launch => (
+              <Box
+                key={launch.flight_number + launch.mission_name}
+                bg="gray.100"
+                mb="0.5rem"
+              >
+                <Flex px="1rem">
+                  <Text fontSize="xl" py="1rem">
+                    Mission <strong >{launch.mission_name}</strong> ( {launch.launch_year} )
+                  </Text>
+
+                  <Spacer />
+
+                  <Tag 
+                    colorScheme={ launch.launch_success? 'green' : 'red' } 
+                    boxSize="4rem"
+                    px="0.5rem"
+                  >
+                    { launch.launch_success? 'Success' : 'Failed' }
+                  </Tag>
+                </Flex>
+
+              </Box>
+            ))
+          }
+        </section>
+      </Box>
+    </>
+  )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from '@chakra-ui/react'
+// import { CounterApp } from "./components/CounterApp";
+// import FirstApp from "./components/FirstApp";
 import { AppLaunches } from "./components/AppLaunches";
-import { CounterApp } from "./components/CounterApp";
-import FirstApp from "./components/FirstApp";
 import './assets/css/styles.css'
 
 
 ReactDOM.createRoot( document.getElementById('root' ) ).render(
     <React.StrictMode>
         {/* <FirstApp name='Gardus' number={ 27 } /> */}
-        
         {/* Ejemplo para default Props */}
         {/* <FirstApp  /> */}
-
         {/* <CounterApp value={ 27 }/> */}
 
-        <AppLaunches />
+        <ChakraProvider>
+            <AppLaunches />
+        </ChakraProvider>
 
     </React.StrictMode>
 )
